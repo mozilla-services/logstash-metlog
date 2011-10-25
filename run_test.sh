284e3ec18f73156b26a38822e2142250e33d3ae3
Victor Ng <crankycoder@gmail.com>
2011-10-25 10:03:44 -0400

    updated run_test.sh to only use the 'real' logstash jar file and point it to the root directory of the plugin path to find tests and plugin code

--- run_test.sh
+++ run_test.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-java -jar logstash-custom.jar test --pluginpath /tmp
+##java -jar lib/logstash-1.1.0beta4-monolithic.jar test -vvv --pluginpath src
+java -jar lib/logstash-1.1.0beta4-monolithic.jar test --pluginpath src
