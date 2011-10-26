2334069d323be7d9ccff52b0a656e716dceb0eea
Victor Ng <crankycoder@gmail.com>
2011-10-26 10:02:56 -0400

    commented the metlog fixture data, dropped an unused tcp input plugin and the old stub for the zeromq input plugin

--- src/logstash/fixtures/metlog.rb
+++ src/logstash/fixtures/metlog.rb
@@ -1,8 +1,11 @@
-# This file contains some helper functions for testing
 require 'logstash'
 
 
 module LogStash::Fixtures
+    # This module is literally just test fixture data
+    # We want to curate sample data that can be reused by multiple
+    # test cases, or at the least have a single place to look when
+    # determining what data we are actually testing with
     class MetlogFixtures
         @@stack_error = {
             'timestamp' => '2011-10-13T09:43:44.386392',
