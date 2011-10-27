d3b4f58cb286538fbf0d356f89ce1f6b56116603
Victor Ng <crankycoder@gmail.com>
2011-10-27 14:50:30 -0400

    added pre-canned Event objects for testing the statsd output plugin

--- src/logstash/fixtures/metlog.rb
+++ src/logstash/fixtures/metlog.rb
@@ -1,5 +1,7 @@
 require 'logstash'
 
+require "logstash/event"
+
 
 module LogStash::Fixtures
     # This module is literally just test fixture data
@@ -15,5 +17,74 @@ module LogStash::Fixtures
             'severity' => 0,
             'message' => 'some log text',
         }
+
+
+        public
+        def self.stack_error
+            @@stack_error
+        end
+
+        @@stackerror_event = nil
+        public
+        def self.stackerror_event
+            event = LogStash::Event.new
+            event.fields['timestamp'] = '2011-10-13T09:43:44.386392'
+            event.fields['fields'] = {'some_data' => 'foo' }
+            event.fields['type'] = 'error'
+            event.fields['logger'] = 'stacktrace'
+            event.fields['severity'] = 0
+            event.fields['payload'] = 'some log text'
+            return event
+        end
+
+
+        # This is a basic increment
+        public
+        def self.simple_incr_counter
+            evt = LogStash::Event.new
+            evt.fields['logger'] = 'incr-space'
+            evt.fields['fields'] = {}
+            evt.fields['fields']['name'] = 'myapp'
+            evt.fields['payload'] = '2'
+            evt.fields['fields']['rate'] = 1
+            return evt
+        end
+
+        # This is a sampled increment
+        public
+        def self.sampled_incr_counter
+            evt = LogStash::Event.new
+            evt.fields['logger'] = 'incr-space'
+            evt.fields['fields'] = {}
+            evt.fields['fields']['name'] = 'my_sampleapp'
+            evt.fields['payload'] = 7
+            evt.fields['fields']['rate'] = 0.15
+            return evt
+        end
+
+        # This is a timed event
+        public
+        def self.timing_event
+            evt = LogStash::Event.new
+            evt.fields['logger'] = 'timing-space'
+            evt.fields['fields'] = {}
+            evt.fields['fields']['name'] = 'my_timing'
+            evt.fields['payload'] = 7
+            evt.fields['fields']['rate'] = 1
+            return evt
+        end
+
+        # This is a sampled timed event
+        public
+        def self.timing_event
+            evt = LogStash::Event.new
+            evt.fields['logger'] = 'timing-space'
+            evt.fields['fields'] = {}
+            evt.fields['fields']['name'] = 'my_sampletiming'
+            evt.fields['payload'] = 7
+            evt.fields['fields']['rate'] = 0.15
+            return evt
+        end
+
     end
 end
