5551ec16f912026b84fc52f1864134e1c0546588
Victor Ng <crankycoder@gmail.com>
2012-04-16 14:57:55 -0400

    added a zeromq handshake input plugin

--- src/logstash/inputs/zeromq_hs.rb
+++ src/logstash/inputs/zeromq_hs.rb
@@ -2,7 +2,7 @@ require "logstash/inputs/base"
 require "logstash/namespace"
 require "timeout"
 
-# Respond to handshaking messages over 0mq using a REQ/REPL socket
+# Read events over a 0MQ SUB socket.
 #
 # You need to have the 0mq 2.1.x library installed to be able to use
 # this input plugin.
@@ -12,11 +12,15 @@ require "timeout"
 #
 class LogStash::Inputs::ZeroMQHandshake < LogStash::Inputs::Base
 
-  config_name "zeromq_handshake"
+  config_name "zeromq_hs"
   plugin_status "experimental"
 
   # 0mq socket address to connect or bind to
-  config :address, :validate => :string, :default => "tcp://127.0.0.1:2121"
+  config :address, :validate => :string, :default => "tcp://127.0.0.1:2120"
+
+  # 0mq topic (Used with ZMQ_SUBSCRIBE, see http://api.zeromq.org/2-1:zmq-setsockopt 
+  # for 'ZMQ_SUBSCRIBE: Establish message filter')
+  config :queue, :validate => :string, :default => "" # default all
 
   # wether to bind ("server") or connect ("client") to the socket
   config :mode, :validate => [ "server", "client"], :default => "client"
@@ -28,15 +32,12 @@ class LogStash::Inputs::ZeroMQHandshake < LogStash::Inputs::Base
     require "ffi-rzmq"
     require "logstash/util/zeromq"
     self.class.send(:include, LogStash::Util::ZeroMQ)
-
-    @socket = context.socket(ZMQ::REP)
-    error_check(@socket.setsockopt(ZMQ::LINGER, 1),
-                "while setting ZMQ::LINGER == 1)")
-    setup(@socket, @address)
+    @subscriber = context.socket(ZMQ::REP)
+    setup(@subscriber, @address)
   end # def register
 
   def teardown
-    error_check(@socket.close, "while closing the zmq socket")
+    error_check(@subscriber.close, "while closing the zmq socket")
   end # def teardown
 
   def server?
@@ -46,15 +47,14 @@ class LogStash::Inputs::ZeroMQHandshake < LogStash::Inputs::Base
   def run(output_queue)
     begin
       loop do
-        # do a ping/pong
-        @socket.recv_string()
-        @socket.send('')
+        puts ("Got: [" + @subscriber.recv() + "]")
+        @subscriber.send('')
       end
     rescue => e
-      @logger.debug("ZMQ Error", :socket => @socket,
+      @logger.debug("ZMQ Error", :subscriber => @subscriber,
                     :exception => e, :backtrace => e.backtrace)
     rescue Timeout::Error
       @logger.debug("Read timeout", subscriber => @subscriber)
     end # begin
   end # def run
-end # class LogStash::Inputs::ZeroMQ
+end # class LogStash::Inputs::ZeroMQHandshake
