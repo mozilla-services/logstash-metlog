704dec6f3751c9b56a51a507e877c4c2fd4781c2
Victor Ng <victor@crankycoder.com>
2012-07-03 10:14:31 -0400

    cleaned up the HDFS upload script to use an INI file for configuration

--- logrotate/upload_log.py
+++ logrotate/upload_log.py
@@ -4,88 +4,112 @@ import subprocess
 import shutil
 import os
 import sys
-from metlog.config import client_from_text_config 
-import ConfigParser
+from metlog.config import client_from_dict_config
+from ConfigParser import SafeConfigParser
 
-#sender_class = metlog.senders.ZmqPubSender 
-#sender_bindstrs = tcp://127.0.0.1:5565 
-cfg_txt = """[metlog] 
-logger = metlog_hadoop_transport
-sender_class = metlog.senders.StdOutSender
-""" 
 
-HADOOP_USER = 'sync_dev'
-HADOOP_HOST = 'research-gw.research.metrics.scl3.mozilla.com'
+class HDFSUploader(object):
+    def __init__(self, cfg):
+        self._cfg = cfg
 
-SDATE = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-DST_FNAME="hadoop_logs/metrics_hdfs.log.%s" % SDATE
+        self.SDATE = datetime.datetime.utcnow().strftime('%Y%m%d_%H%M%S')
 
-SRC_LOGFILE = '/var/log/sync/metrics_hdfs.log.1'
+        self.HADOOP_USER = cfg.get('metlog_metrics_hdfs', 'HADOOP_USER')
+        self.HADOOP_HOST = cfg.get('metlog_metrics_hdfs', 'HADOOP_HOST')
+        self.SRC_LOGFILE = cfg.get('metlog_metrics_hdfs', 'SRC_LOGFILE')
 
-# Make a copy of the log file in case it gets rotated out from under us
-LOCAL_FNAME="/opt/metlog/hdfs_logs/metrics_hdfs.log.%s" % SDATE
+        self.DST_FNAME = "%s.%s" % (cfg.get('metlog_metrics_hdfs',
+            'DST_FNAME'), self.SDATE)
 
-#############################
-#############################
-#############################
+        # Make a copy of the log file in case it gets rotated out from
+        # under us
+        self.LOCAL_FNAME = "/opt/metlog/hdfs_logs/metrics_hdfs.log.%s"
+        self.LOCAL_FNAME = self.LOCAL_FNAME % self.SDATE
 
-LOGGER = client_from_text_config(cfg_txt, 'metlog') 
+        self.ERR_RM_HDFS = 'Failed to remove [%s] from %s'
+        self.ERR_RM_HDFS = self.ERR_RM_HDFS % \
+                            (self.DST_FNAME, self.HADOOP_HOST)
 
-def remove_file_from_hadoop():
-    rm_cmd = ["ssh", 
-      "%s@%s" % (HADOOP_USER, HADOOP_HOST),
-      "rm", 
-      DST_FNAME]
-    # try to clean up the file off of the metrics server 
-    print ' '.join(rm_cmd)
-    rm_result = subprocess.call(rm_cmd)
-    if rm_result != 0:
-        LOGGER.error('Failed to remove [%s] from %s after DFS write failure' % (DST_FNAME, HADOOP_HOST))    
+        self.ERR_XFER_HADOOP = "Transport of [%s] to HDFS failed"
+        self.ERR_XFER_HADOOP = self.ERR_XFER_HADOOP % self.DST_FNAME
 
-def cleanup_local():
-    try:
-        os.remove(LOCAL_FNAME)
-    except Exception, e:
-        LOGGER.error("Error removing: [%s]: %s" % (LOCAL_FNAME, e))
+        self.ERR_REMOVE_LOCAL = "Error removing: [%s]" % self.LOCAL_FNAME
 
-def copy_log_local():
-    try:
-        shutil.copy(SRC_LOGFILE, LOCAL_FNAME)
-    except:
-        LOGGER.error("Error copying JSON log file for processing")
-        sys.exit(1)
+        self.ERR_DFS_WRITE = "DFS Write failure for [%s]" % self.DST_FNAME
 
-def push_to_hadoop_host():
-    scp_cmd = ["scp",
-               LOCAL_FNAME,
-              "%s@%s:%s" % (HADOOP_USER, HADOOP_HOST, DST_FNAME),]
-    print ' '.join(scp_cmd)
-    scp_result = subprocess.call(scp_cmd)
+        self.LOGGER = client_from_dict_config(dict(cfg.items('metlog')))
 
-    if scp_result != 0:
-        LOGGER.error("Transport to HDFS cluster failed.  Filename: [%s]" % DST_FNAME)
-        sys.exit(1)
+    def remove_file_from_hadoop(self):
+        rm_cmd = ["ssh",
+          "%s@%s" % (self.HADOOP_USER, self.HADOOP_HOST),
+          "rm",
+          self.DST_FNAME]
+        # try to clean up the file off of the metrics server
+        print ' '.join(rm_cmd)
+        rm_result = subprocess.call(rm_cmd)
+        if rm_result != 0:
+            self.LOGGER.error(self.ERR_RM_HDFS)
 
-def dfs_put():
-    # Just tell hadoop to import the file
-    cmd = ["ssh", 
-      "%s@%s" % (HADOOP_USER, HADOOP_HOST),
-      "hadoop", 
-      "dfs", 
-      "-put", 
-      DST_FNAME, 
-      "/user/%s/%s" % (HADOOP_USER, DST_FNAME)]
+    def cleanup_local(self):
+        try:
+            os.remove(self.LOCAL_FNAME)
+        except:
+            self.LOGGER.error(self.ERR_REMOVE_LOCAL)
 
-    dfs_result = subprocess.call(cmd)
+    def copy_log_local(self):
+        try:
+            shutil.copy(self.SRC_LOGFILE, self.LOCAL_FNAME)
+        except:
+            self.LOGGER.error("Error copying JSON log file for processing")
+            sys.exit(1)
 
-    if dfs_result != 0:
-        remove_file_from_hadoop()
-        LOGGER.error("DFS Write failure for [%s]" % DST_FNAME)
-        sys.exit(1)
-    else:
-        remove_file_from_hadoop()
-        cleanup_local()
+    def push_to_hadoop_host(self):
+        scp_cmd = ["scp",
+                   self.LOCAL_FNAME,
+                  "%s@%s:%s" % (self.HADOOP_USER, self.HADOOP_HOST,
+                      self.DST_FNAME)]
+        scp_result = subprocess.call(scp_cmd)
 
-copy_log_local()
-push_to_hadoop_host()
-dfs_put()
+        if scp_result != 0:
+            self.LOGGER.error(self.ERR_XFER_HADOOP)
+            sys.exit(1)
+
+    def dfs_put(self):
+        # Just tell hadoop to import the file
+        cmd = ["ssh",
+          "%s@%s" % (self.HADOOP_USER, self.HADOOP_HOST),
+          "hadoop",
+          "dfs",
+          "-put",
+          self.DST_FNAME,
+          "/user/%s/%s" % (self.HADOOP_USER, self.DST_FNAME)]
+
+        dfs_result = subprocess.call(cmd)
+
+        if dfs_result != 0:
+            self.remove_file_from_hadoop()
+            self.LOGGER.error(self.ERR_DFS_WRITE)
+            sys.exit(1)
+        else:
+            self.remove_file_from_hadoop()
+            self.cleanup_local()
+
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description="Upload JSON logs to HDFS")
+    parser.add_argument('--config', type=argparse.FileType('r'),
+            required=True)
+
+    parsed_args = parser.parse_args()
+
+    cfg = SafeConfigParser()
+    cfg.readfp(parsed_args.config)
+
+    uploader = HDFSUploader(cfg)
+    uploader.copy_log_local()
+    uploader.push_to_hadoop_host()
+    uploader.dfs_put()
+
+if __name__ == '__main__':
+    main()
